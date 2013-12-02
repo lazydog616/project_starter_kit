a69ce62bceb01d2b16786ed495e1abe71c1aa90c
lazydog616 <xiaozeng@seas.upenn.edu>
2013-12-02 18:24:48 -0500

    fix para_generate bug

--- starter_kit/code/test_svm_with_crossvalidation.m
+++ starter_kit/code/test_svm_with_crossvalidation.m
@@ -7,7 +7,7 @@ load ../data/x_newquiz.mat
 %Xq_counts = quiz.counts;
 %[ para ] = find_opt_para( Xt_counts,Yt);   
 %[ para ] = find_opt_para_libsvm(Xt_counts, Yt);
-para_matrix = generate_para_matrix([0.1 0.1 0.1 0.1 0.1], [2.3, 2.3, 2.3, 2.3, 2.3], [0.3 0.3 0.3 0.3 0.3]);
+para_matrix = generate_para_matrix([1 1 1 1 1], [2, 2, 2, 2, 1], [0.5 0.5 0.5 0.5 0.5]);
 [searchmin,fval,fval_v] = find_opt_para_grid_search(Xt_counts, Yt, para_matrix);
 % model = train(Yt(1:20000),Xt_counts(1:20000,:),sprintf('-c 1 -q -w1 %g -w2 %g -w3 %g -w4 %g -w5 %g',0.1165, 0.2814, 0.3468, 0.3237, 0.1078));
 % % 
